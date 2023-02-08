8c272afb13bcd03147cbca6caf44b34d810610e1
DaniloHY <nilo.11.dhy@gmail.com>
2023-02-07 21:22:48 -0300

    Adapter

--- app/src/main/java/br/com/etecia/drinks/MainActivity.java
+++ app/src/main/java/br/com/etecia/drinks/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.etecia.drinks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idLista = findViewById(R.id.idLista);
+
+        MyAdapter adapter = new MyAdapter();
+        idLista.setAdapter(adapter);
     }
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
 }
